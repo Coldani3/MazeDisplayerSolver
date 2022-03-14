5bbbcd457beed48a1a618fe67f1d53d0909ea105
Coldani3 <p110117069@gmail.com>
2022-03-14 11:56:28 +0000

    Add more overrides, restructure things, start implementing file reading.

--- src/Includes/Maze/Maze.h
+++ src/Includes/Maze/Maze.h
@@ -1,20 +1,39 @@
 #include <string>
 #include <iostream>
+#include <vector>
 
 #pragma once
-class Maze
-{
-public:
-	Maze();
-	Maze(int dimensions[]);
-	~Maze();
-	void loadFromFile(std::string fileName);
-private:
-	int width;
-	int height;
-	int depth;
-	int hyperDepth;
-	//i'm not bothering with higher than 4D here, bytes are fine
-	std::vector<char> mazeData;
+
+//The flags each bit of each maze cell contain.
+enum MazeDirections {
+	RIGHT = 1,
+	LEFT = 2,
+	UP = 4,
+	DOWN = 8,
+	FORWARD = 16,
+	BACKWARD = 32,
+	ANA = 64,
+	KATA = 128
 };
 
+//A maze that stores the data and allows access of mazes up to and including 4 dimensions.
+class Maze {
+public:
+	int width = 0;
+	int height = 0;
+	int depth = 0;
+	int hyperDepth = 0;
+	int dimensions;
+	std::vector<int> sizes;
+
+	Maze(std::vector<int> dimensions);
+	~Maze();
+	void loadFromFile(std::string fileName);
+	//access individual bytes
+	char operator[] (std::vector<int> coordinates) const;
+	char& operator[](std::vector<int> coordinates);
+
+protected:
+	//The data that makes up the maze.
+	std::vector<char> mazeData;
+};
--- src/Src/Maze/Maze.cpp
+++ src/Src/Maze/Maze.cpp
@@ -2,21 +2,123 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <array>
+#include <exception>
 
-Maze::Maze() {
-
+bool isNumberChar(char character) {
+	return (character >= 48 && character <= 57);
 }
 
-Maze::Maze(int dimensions[]) {
+Maze::Maze(std::vector<int> dimensionsVec) {
+	int length = 1;
+	int dimensionsSize = dimensionsVec.size();
 
+	if (dimensionsSize < 2) {
+		try {
+			throw dimensionsSize;
+		} catch (int e) {
+			std::cerr << "A maze cannot have less than 2 dimensions (was " << e << ")" << std::endl;
+			throw;
+		}
+	}
+
+	for (int i = 0; i < dimensionsSize; i++) {
+		length *= dimensionsVec[i];
+	}
+
+	width = dimensionsVec[0];
+	height = dimensionsVec[1];
+
+	dimensions = 2;
+
+	if (dimensionsSize > 2) {
+		depth = dimensionsVec[2];
+		dimensions = 3;
+	}
+
+	if (dimensionsSize > 3) {
+		hyperDepth = dimensionsVec[3];
+		dimensions = 4;
+	}
+
+	mazeData = std::vector<char>(length);
+	sizes = dimensionsVec;
 }
 
 Maze::~Maze() {
-	delete mazeData;
+	
 }
 
 void Maze::loadFromFile(std::string fileName) {
 	std::ifstream inFile;
 
 	inFile.open(fileName);
+
+	//get cell size
+	char currentChar;
+	int cellSize = 0;
+	inFile.get(currentChar);
+
+	while (currentChar != '{') {
+		if (isNumberChar(currentChar)) {
+			cellSize *= 10;
+			cellSize += (int)currentChar;
+			inFile.get(currentChar);
+		} else {
+			throw "Malformed maze file cell size in " + fileName;
+		}
+	}
+
+	//get dimensions
+	int dimSizeCounter = 0;
+	std::vector<int> dimSizes = std::vector<int>();
+	inFile.get(currentChar);
+	dimSizes.push_back(0);
+
+	while (currentChar != '}') {
+		while (currentChar != ',' && currentChar != '}') {
+			if (!(dimSizes.size() >= dimSizeCounter + 1)) {
+				dimSizes.push_back(0);
+			}
+
+			if (isNumberChar(currentChar)) {
+				dimSizes[dimSizeCounter] *= 10;
+				dimSizes[dimSizeCounter] += (int)currentChar;
+				inFile.get(currentChar);
+			} else {
+				throw "Malformed maze file dimension specification in " + fileName;
+			}
+		}
+
+		dimSizeCounter++;
+	}
+
+	while (!inFile.eof()) {
+		inFile.get(currentChar);
+
+
+	}
+
+	inFile.close();
+}
+
+char Maze::operator[](std::vector<int> coordinates) const {
+	std::array<int, 4> coordsToFour = { 0, 0, 0, 0 };
+
+	for (int i = 0; i < coordinates.size(); i++) {
+		coordsToFour[i] = coordinates[i];
+	}
+
+	return mazeData[(height * width * depth * coordsToFour[3]) + (height * width * coordsToFour[2]) + (height * coordsToFour[1]) + coordsToFour[0]];
+}
+
+char& Maze::operator[](std::vector<int> coordinates) {
+	std::array<int, 4> coordsToFour = { 0, 0, 0, 0 };
+
+	for (int i = 0; i < coordinates.size(); i++) {
+		coordsToFour[i] = coordinates[i];
+	}
+
+	return mazeData[(height * width * depth * coordsToFour[3]) + (height * width * coordsToFour[2]) + (height * coordsToFour[1]) + coordsToFour[0]];
 }
