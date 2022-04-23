44f5b1a96cfea9beb20e92585a13dd8fe13a3c03
Coldani3 <p110117069@gmail.com>
2022-04-23 02:05:55 +0100

    Start working on simpleneuralnetworksolver so I have something for the deadline.

---
+++ src/Includes/Solvers/SimpleNeuralNetworkSolver.h
@@ -0,0 +1,25 @@
+#include <Solvers/Solver.h>
+#include <vector>
+#include <Solvers/NeuralNetwork/NeuralNetwork.h>
+#include <Maze/Maze.h>
+
+#pragma once
+class SimpleNeuralNetworkSolver : public Solver
+{
+public:
+	SimpleNeuralNetworkSolver(std::vector<unsigned int> nodesPerLayer, int networks, float learnRate, Maze maze);
+	void solve();
+	void train(NeuralNetwork network, std::vector<std::vector<int>> path);
+
+	std::vector<std::vector<std::vector<int>>> queryNetworkPaths(std::vector<int> startPath);
+	std::vector<float> networkInputForCoords(std::vector<int> coords);
+	std::vector<int> addCoords(std::vector<int> coords1, std::vector<int> coords2);
+	std::vector<int> minusCoords(std::vector<int> coords1, std::vector<int> coords2);
+	std::vector<int> intify(std::vector<float> vec);
+	std::vector<float> floatify(std::vector<int> vec);
+
+private:
+	std::vector<NeuralNetwork> networks;
+	Maze maze;
+};
+
---
+++ src/Src/Solvers/SimpleNeuralNetworkSolver.cpp
@@ -0,0 +1,119 @@
+#include <Solvers/SimpleNeuralNetworkSolver.h>
+#include <algorithm>
+
+
+const int maxQueries = 4;
+const int trainsPerCycle = 50;
+
+SimpleNeuralNetworkSolver::SimpleNeuralNetworkSolver(std::vector<unsigned int> nodesPerLayer, int networks, float learnRate, Maze maze) {
+	this->maze = maze;
+	
+	for (int i = 0; i < networks; i++) {
+		this->networks.push_back(NeuralNetwork(nodesPerLayer, learnRate));
+	}
+}
+
+std::vector<float> SimpleNeuralNetworkSolver::networkInputForCoords(std::vector<int> coords) {
+	unsigned int cellData = maze[coords];
+
+	std::vector<float> out(12);
+
+	for (int i = 0; i < 4; i++) {
+		out.push_back(coords[i]);
+	}
+
+	for (int i = 0; i < 8; i++) {
+		out.push_back((cellData & (1 << i)) >> i);
+	}
+
+	return out;
+}
+
+std::vector<int> SimpleNeuralNetworkSolver::addCoords(std::vector<int> coords1, std::vector<int> coords2) {
+	std::vector<int> out;
+
+	for (int i = 0; i < 4; i++) {
+		out.push_back(coords1[i] + coords2[i]);
+	}
+
+	return out;
+}
+
+std::vector<float> SimpleNeuralNetworkSolver::floatify(std::vector<int> vec) {
+	std::vector<float> out(vec.size());
+
+	for (int i = 0; i < vec.size(); i++) {
+		out[i] = (float)vec[i];
+	}
+
+	return out;
+}
+
+std::vector<int> SimpleNeuralNetworkSolver::intify(std::vector<float> vec) {
+	std::vector<int> out(vec.size());
+
+	for (int i = 0; i < vec.size(); i++) {
+		out[i] = (int)vec[i];
+	}
+
+	return out;
+}
+
+std::vector<int> SimpleNeuralNetworkSolver::minusCoords(std::vector<int> coords1, std::vector<int> coords2) {
+	std::vector<int> out;
+
+	for (int i = 0; i < 4; i++) {
+		out.push_back(coords1[i] - coords2[i]);
+	}
+
+	return out;
+}
+
+std::vector<std::vector<std::vector<int>>> SimpleNeuralNetworkSolver::queryNetworkPaths(std::vector<int> startPath) {
+	//network -> list of coords -> each coord object
+	std::vector<std::vector<std::vector<int>>> paths;
+
+	//add start path to start of all paths
+	for (int i = 0; i < networks.size(); i++) {
+		paths[i].push_back(startPath);
+	}
+
+	for (int network = 0; network < networks.size(); network++) {
+		for (int query = 0; query < maxQueries; query++) {
+			//get current coordinate
+			std::vector<int> difference = NeuralNetwork::normalize(networks[network].query(networkInputForCoords(paths[network][query])));
+			paths[network].push_back(addCoords(paths[network][query], difference));
+		}
+	}
+}
+
+void SimpleNeuralNetworkSolver::solve() {
+	//query all networks and pick the best path
+	std::vector<std::vector<std::vector<int>>> paths = queryNetworkPaths(maze.mazeEntrance);
+	std::vector<float> successes(paths.size());
+
+	//all paths are the same length so the closest should be the best path.
+	for (int i = 0; i < paths.size(); i++) {
+		std::vector<int> lastCoord = paths[i].back();
+
+		//4D pythagoras = sqrt(a^2 + b^2 + c^2 + d^2)
+		float distance = sqrtf(pow(lastCoord[0], 2) + pow(lastCoord[1], 2) + pow(lastCoord[2], 2) + pow(lastCoord[3], 2));
+
+		successes[i] = distance;
+	}
+
+	std::vector<std::vector<int>> mostSuccessfulPath = paths[std::distance(successes.begin(), std::max_element(successes.begin(), successes.end()))];
+
+	//use most successful path to train AIs
+	
+}
+
+void SimpleNeuralNetworkSolver::train(NeuralNetwork network, std::vector<std::vector<int>> path) {
+	for (int i = 0; i < path.size() - 1; i++) {
+		std::vector<int> expected = minusCoords(path[i + 1], path[i]);
+
+		std::vector<float> input = networkInputForCoords(expected);
+
+		network.train(input, floatify(expected));
+	}
+}
